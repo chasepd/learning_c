63e28be2ae1b5f236d1e581a1f9a61f1d17112c0
Chase Davies <chase.davies@sproutsocial.com>
2021-03-22 16:55:49 -0600

    Solved FizzBuzz challenge

---
+++ fizzbuzz/src/fizzbuzz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	for ( int number = 1; number <= 100; number++ ){
+		if (number % 3 == 0){
+			if( number % 5 == 0 ){
+				printf("FizzBuzz\n");
+			}
+			else{
+				printf("Fizz\n");
+			}
+		}
+		else if (number % 5 == 0){
+			printf("Buzz\n");
+		}
+		else{
+			printf("%i\n", number);
+		}
+	} 
+	return 0;
+}
