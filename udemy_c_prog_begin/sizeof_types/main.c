32c2b5d032d7203294c889867048ccd41b9095b8
Chase Davies <chase.davies@sproutsocial.com>
2021-06-22 15:31:09 -0600

    finished sizeof challenge

---
+++ udemy_c_prog_begin/sizeof_types/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    
+    printf("%lu\n", sizeof(int));
+    printf("%lu\n", sizeof(short int));
+    printf("%lu\n", sizeof(long int));
+    printf("%lu\n", sizeof(short unsigned int));
+    printf("%lu\n", sizeof(unsigned int));
+    printf("%lu\n", sizeof(long unsigned int));
+    printf("%lu\n", sizeof(float));
+    printf("%lu\n", sizeof(long));
+    printf("%lu\n", sizeof(char));
+    return 0;
+}
